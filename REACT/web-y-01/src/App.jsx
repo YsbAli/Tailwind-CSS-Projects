1b6817bb981286fe3f32ea86f10e695eaa168d29
Ysb <97525491+YsbAli@users.noreply.github.com>
2023-12-09 13:12:08 +0530

    navbar , search input, cart-button added

--- REACT/web-y-01/src/App.jsx
+++ REACT/web-y-01/src/App.jsx
@@ -1,7 +1,11 @@
+import Navbar from "./Components/Navbar";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <div>
+        <Navbar />
+      </div>
     </>
   );
 };
---
+++ REACT/web-y-01/src/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+        {/* left side Starts here*/}
+        <div className="flex items-center">
+          <div className="cursor-pointer">
+            <AiOutlineMenu size={30} />
+          </div>
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2 ">
+            Best <span className="font-bold">Eats</span>
+          </h1>
+          <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+            <p className="bg-black text-white rounded-full p-2">Delivery</p>
+            <p className="p-2">Pickup</p>
+          </div>
+        </div>
+        {/* Search Bar starts */}
+
+        <div className="flex  bg-gray-200 rounded-full items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+          <AiOutlineSearch size={20} />
+          <input
+            className="bg-transparent w-full p-2 focus:outline-none"
+            type="text"
+            placeholder="Search Food here..."
+          />
+        </div>
+
+        {/* Cart Button */}
+
+        <button className="bg-black text-white  hidden md:flex items-center py-2 rounded-full">
+          <BsFillCartFill className="mr-2" size={18} />
+          Cart
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
