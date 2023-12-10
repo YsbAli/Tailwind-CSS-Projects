e33f4e937017cc3d667ec19c305ba241d7fce84a
Ysb <97525491+YsbAli@users.noreply.github.com>
2023-12-10 14:33:19 +0530

    Cards section are added

--- REACT/web-y-01/src/App.jsx
+++ REACT/web-y-01/src/App.jsx
@@ -1,3 +1,4 @@
+import Card from "./Components/Card";
 import HeroSection from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
 
@@ -7,6 +8,7 @@ const App = () => {
       <div>
         <Navbar />
         <HeroSection />
+        <Card title="Sun's Out BOGO'S Out" text="Through B/26" />
       </div>
     </>
   );
